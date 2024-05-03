2c924a1796ec87081fc4fa80477a231f0256f334
VijuVinodKannaR <167547825+VijuVinodKannaR@users.noreply.github.com>
2024-05-03 13:35:38 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -45,7 +45,7 @@ def microsoft_teams():
 
         my_bar = st.progress(0, text=progress_text)
         # Execute the PowerShell command to upgrade the application as admin
-        subprocess.run(["start", "powershell", "-Command", "winget upgrade 'Microsoft Edge' --disable-interactivity --authentication-mode silent"], shell=Fasle, check=True)
+        subprocess.run(["start", "powershell", "-Command", "winget upgrade 'Microsoft Edge' --disable-interactivity --authentication-mode silent"], shell=True, check=True)
         for percent_complete in range(100):
             time.sleep(0.01)
             my_bar.progress(percent_complete + 1, text=progress_text)
