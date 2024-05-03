813d93bab70fb58e6cfa9c1bb33a08f3dde1b457
VijuVinodKannaR <167547825+VijuVinodKannaR@users.noreply.github.com>
2024-05-03 12:46:07 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,77 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import time
+import tkinter
+import subprocess
 
-LOGGER = get_logger(__name__)
+session_state = st.session_state
+
+def google_chrome():
+    try:
+        progress_text = "Checking for updates... Please wait..."
+
+        my_bar = st.progress(0, text=progress_text)
+
+        subprocess.run(["winget", "upgrade", "remote desktop", "--disable-interactivity"], check=True)
+
+        for percent_complete in range(100):
+            time.sleep(0.01)
+            my_bar.progress(percent_complete + 1, text=progress_text)
+        time.sleep(1)
+        my_bar.empty()
+        st.snow()
+        st.success("Application updated successfully.")
+    except subprocess.CalledProcessError as e:
+        my_bar.empty()
+        st.error(f"No New Updates Available")
+
+def microsoft_edge():
+    try:
+        progress_text = "Checking for updates... Please wait..."
+
+        my_bar = st.progress(0, text=progress_text)
+        subprocess.run(["winget", "upgrade", "Edge", "--disable-interactivity"], check=True)
+        for percent_complete in range(100):
+            time.sleep(0.01)
+            my_bar.progress(percent_complete + 1, text=progress_text)
+        time.sleep(1)
+        my_bar.empty()
+        st.snow()
+        st.success("Application updated successfully.")
+    except subprocess.CalledProcessError as e:
+        my_bar.empty()
+        st.error(f"No New Updates Available")
+def microsoft_teams():
+    try:
+        progress_text = "Checking for updates... Please wait..."
+
+        my_bar = st.progress(0, text=progress_text)
+        # Execute the PowerShell command to upgrade the application as admin
+        subprocess.run(["winget", "upgrade", "Microsoft.Teams", "--disable-interactivity"], check=True)
+        for percent_complete in range(100):
+            time.sleep(0.01)
+            my_bar.progress(percent_complete + 1, text=progress_text)
+        time.sleep(1)
+        my_bar.empty()
+        st.snow()
+        st.success("Application updated successfully.")
+    except subprocess.CalledProcessError as e:
+        my_bar.empty()
+        st.error(f"No New Updates Available")
+
+# Streamlit app setup
+
+def main():
+    st.title("Applicaiton Update Manager")
+    st.write("Click on the below buttons to update your application")
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    if st.button("Google Chrome"):
+        google_chrome()
+    if st.button("Microsoft Edge"):
+        microsoft_edge()
+    if st.button("Microsoft Teams"):
+        microsoft_teams()
 
 
 if __name__ == "__main__":
-    run()
+    main()
