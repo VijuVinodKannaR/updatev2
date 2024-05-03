f675cf8fe0f4d52f5cc3a41c56a5bb003e031337
VijuVinodKannaR <167547825+VijuVinodKannaR@users.noreply.github.com>
2024-05-03 13:27:21 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -28,7 +28,7 @@ def microsoft_edge():
         progress_text = "Checking for updates... Please wait..."
 
         my_bar = st.progress(0, text=progress_text)
-        subprocess.run(["start", "cmd", "-Command", "winget upgrade teams --disable-interactivity"],  shell=True, check=True)
+        subprocess.run(["start", "cmd", "-Command", "winget upgrade 'Microsoft Edge' --disable-interactivity --authentication-mode silent"],  shell=True, check=True)
         for percent_complete in range(100):
             time.sleep(0.01)
             my_bar.progress(percent_complete + 1, text=progress_text)
